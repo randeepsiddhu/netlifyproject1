76cf4d412b4658d37f483812ef7994b72ddc6e9d
randeepsiddhu <randeepsiddhu@gmail.com>
2024-12-10 14:18:19 -0800

    Create static-response.js

---
+++ functions/static-response.js
@@ -0,0 +1,17 @@
+exports.handler = async (event, context) => {
+  if (event.httpMethod === "POST") {
+    // Return a static response to the POST request
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        status: "success",
+        message: "This is a static response from Netlify Functions.",
+      }),
+    };
+  } else {
+    return {
+      statusCode: 405,
+      body: "Method Not Allowed",
+    };
+  }
+};
