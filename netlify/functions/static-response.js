af1e9f6ce1c47ae86f941a88b3cf8c3ca5c30b2f
randeepsiddhu <randeepsiddhu@gmail.com>
2024-12-16 18:51:57 -0800

    multiple endpoints

--- netlify/functions/static-response.js
+++ netlify/functions/static-response.js
@@ -1,13 +1,43 @@
-const response = require("../data/banner_response1.json");
+const bannerResponse1 = require("../data/banner_response1.json");
+const bannerResponse2 = require("../data/banner_response2.json");
 
 exports.handler = async (event, context) => {
-  if (event.httpMethod === "POST") {
-    // Return a static response to the POST request
-    return {
-      statusCode: 200,
-      body: JSON.stringify(response),
-    };
-  } else {
+  const { httpMethod, path } = event;
+
+  // Check for the request's path and method
+  if (httpMethod === "POST") {
+    if (path === "/api/banner1") {
+      return {
+        statusCode: 200,
+        body: JSON.stringify(bannerResponse1),
+      };
+    } else if (path === "/api/banner2") {
+      return {
+        statusCode: 200,
+        body: JSON.stringify(bannerResponse2),
+      };
+    } else {
+      return {
+        statusCode: 404,
+        body: "Endpoint not found",
+      };
+    }
+  } 
+  else if (httpMethod === "GET") {
+    if (path === "/api/banner1") {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "GET: Banner endpoint" }),
+      };
+    } else {
+      return {
+        statusCode: 404,
+        body: "GET endpoint not found",
+      };
+    }
+  }
+  // Handle other HTTP methods (PUT, DELETE, etc.)
+  else {
     return {
       statusCode: 405,
       body: "Method Not Allowed",
